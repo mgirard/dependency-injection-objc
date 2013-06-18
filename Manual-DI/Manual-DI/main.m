a64b530f28281d3e001d3d7d2bd6bb99ab633c12
Michael Girard <michael.girard@compuware.com>
2013-06-18 11:20:35 -0400

    Adding base files.

---
+++ Manual-DI/Manual-DI/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Manual-DI
+//
+//  Created by Michael Girard on 6/18/13.
+//  Copyright (c) 2013 Compuware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([DIAppDelegate class]));
+	}
+}
